830237c6f1f6041c5e0c0f261e8531010ea964fc
bobthebuilder98765 <124575412+bobthebuilder98765@users.noreply.github.com>
2024-07-23 14:12:08 +0300

    added slashj

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ui import Button, View
 from discord.ext import commands
 import os
@@ -6,9 +7,8 @@ import asyncio
 from webserver import keep_alive
 
 token = os.environ.get('token')
-
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 games = {
     "minecraft": {"style": discord.ButtonStyle.blurple, "role": "Minecraft", "emoji": "⛏️"},
@@ -33,47 +33,58 @@ class GameButtonView(View):
         for game, style in games.items():
             self.add_item(Button(style=style['style'], label=game, custom_id=game, emoji=style['emoji']))
 
-@bot.command()
-async def spawn(ctx):
+@bot.tree.command(name="spawn", description="Spawn game buttons")
+async def spawn(interaction: discord.Interaction):
     view = GameButtonView()
-    await ctx.send("Click a button to find players for a game.", view=view)
+    await interaction.response.send_message("Click a button to find players for a game.", view=view)
 
 @bot.event
 async def on_interaction(interaction: discord.Interaction):
-    if interaction.data['component_type'] == 2:  # 2 corresponds to a button
-        game = interaction.data['custom_id']
-        if interaction.user.voice:
-            voice_channel = interaction.user.voice.channel
-            role_name = games[game]['role']
-            role = discord.utils.get(interaction.guild.roles, name=role_name)
-            if role:
-                users_with_role = [member.mention for member in role.members if member != interaction.user]
-                users_with_role_str = ' '.join(users_with_role)
-                embed = discord.Embed(
-                    title=f"{interaction.user.name} is looking to play {game}",
-                    description=f"{users_with_role_str}\n\n{interaction.user.mention} is waiting at voice: {voice_channel.mention}"
-                )
-
-                channel = bot.get_channel(1156180116024590396)
-                await interaction.response.defer()
-                await channel.send(embed=embed)
+    if interaction.type == discord.InteractionType.component:
+        custom_id = interaction.data.get('custom_id')
+        if custom_id in games:
+            game = custom_id
+            if interaction.user.voice:
+                voice_channel = interaction.user.voice.channel
+                role_name = games[game]['role']
+                role = discord.utils.get(interaction.guild.roles, name=role_name)
+                if role:
+                    users_with_role = [member.mention for member in role.members if member != interaction.user]
+                    users_with_role_str = ' '.join(users_with_role)
+                    embed = discord.Embed(
+                        title=f"{interaction.user.name} is looking to play {game}",
+                        description=f"{users_with_role_str}\n\n{interaction.user.mention} is waiting at voice: {voice_channel.mention}"
+                    )
+                    channel = bot.get_channel(1156180116024590396)
+                    await interaction.response.defer()
+                    await channel.send(embed=embed)
+                else:
+                    await interaction.response.defer()
+                    channel = bot.get_channel(1156180116024590396)
+                    message = await channel.send("You need to be in a voice channel to use this command. " + interaction.user.mention)
+                    await asyncio.sleep(10)
+                    await message.delete()
             else:
                 await interaction.response.defer()
-                channel = bot.get_channel(1156180116024590396)
+                channel = bot.get_channel(1155946685676146709)
                 message = await channel.send("You need to be in a voice channel to use this command. " + interaction.user.mention)
                 await asyncio.sleep(10)
                 await message.delete()
-        else:
-            await interaction.response.defer()
-            channel = bot.get_channel(1155946685676146709)
-            message = await channel.send("You need to be in a voice channel to use this command. " + interaction.user.mention)
-            await asyncio.sleep(10)
-            await message.delete()
 
-@bot.command()
-async def add(ctx, name: str, role: str, emoji: str):
+@bot.tree.command(name="addbutton", description="Add a new game button")
+@app_commands.describe(
+    name="The name of the game",
+    role="The role associated with the game",
+    emoji="The emoji for the game button"
+)
+async def addbutton(interaction: discord.Interaction, name: str, role: str, emoji: str):
     games[name] = {"style": discord.ButtonStyle.blurple, "role": role, "emoji": emoji}
-    await ctx.send(f"Added new game: {name} with role: {role} and emoji: {emoji}")
+    await interaction.response.send_message(f"Added new game button: {name} with role: {role} and emoji: {emoji}")
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    await bot.tree.sync()
 
 keep_alive()
 bot.run(token)
