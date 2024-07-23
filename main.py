4269a60017193334cb69a9debae407e39272eace
bobthebuilder98765 <124575412+bobthebuilder98765@users.noreply.github.com>
2024-07-23 14:06:13 +0300

    Update main.py added /add

--- main.py
+++ main.py
@@ -2,35 +2,36 @@ import discord
 from discord.ui import Button, View
 from discord.ext import commands
 import os
-import asyncio  # Import asyncio module
+import asyncio
 from webserver import keep_alive
 
-# Fill in your token here
 token = os.environ.get('token')
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 games = {
     "minecraft": {"style": discord.ButtonStyle.blurple, "role": "Minecraft", "emoji": "⛏️"},
     "fortnite": {"style": discord.ButtonStyle.blurple, "role": "Fortnite", "emoji": "🌪️"},
     "scribble": {"style": discord.ButtonStyle.blurple, "role": "Scribble", "emoji": "🖍️"},
-    "gartic phone": {"style": discord.ButtonStyle.blurple, "role": "Garatic Phone", "emoji": "📱"},
+    "gartic phone": {"style": discord.ButtonStyle.blurple, "role": "Gartic Phone", "emoji": "📱"},
     "valorant": {"style": discord.ButtonStyle.blurple, "role": "Valorant", "emoji": "🔫"},
     "cs": {"style": discord.ButtonStyle.blurple, "role": "CS:GO", "emoji": "💣"},
-    "Lethal Comapny" : {"style": discord.ButtonStyle.blurple, "role": "Lethal Comapny", "emoji":"👹"},
-    "VR GAMES" : {"style": discord.ButtonStyle.blurple, "role": "VR GAMES", "emoji":"👓"},
-    "Brawlhalla" : {"style": discord.ButtonStyle.blurple, "role": "Brawlhalla", "emoji":"⚔️"},
-    "ARMA III" : {"style": discord.ButtonStyle.blurple, "role": "ARMA III", "emoji":"🪖"},
+    "Lethal Company": {"style": discord.ButtonStyle.blurple, "role": "Lethal Company", "emoji": "👹"},
+    "VR GAMES": {"style": discord.ButtonStyle.blurple, "role": "VR GAMES", "emoji": "👓"},
+    "Brawlhalla": {"style": discord.ButtonStyle.blurple, "role": "Brawlhalla", "emoji": "⚔️"},
+    "ARMA III": {"style": discord.ButtonStyle.blurple, "role": "ARMA III", "emoji": "🪖"},
     "DayZ": {"style": discord.ButtonStyle.blurple, "role": "DayZ", "emoji": "🧟"},
     "Stick Fight: The Game": {"style": discord.ButtonStyle.blurple, "role": "Stick Fight: The Game", "emoji": "👨‍🦯"},
-    "The Finals": {"style": discord.ButtonStyle.blurple, "role": "The Finals", "emoji": "🧱"}
+    "The Finals": {"style": discord.ButtonStyle.blurple, "role": "The Finals", "emoji": "🧱"},
+    "Tank Team": {"style": discord.ButtonStyle.blurple, "role": "Tank Team", "emoji": "🚛"}
 }
 
 class GameButtonView(View):
     def __init__(self):
         super().__init__()
         for game, style in games.items():
-            self.add_item(Button(style=games[game]['style'], label=game, custom_id=game, emoji=games[game]['emoji']))
+            self.add_item(Button(style=style['style'], label=game, custom_id=game, emoji=style['emoji']))
 
 @bot.command()
 async def spawn(ctx):
@@ -48,48 +49,31 @@ async def on_interaction(interaction: discord.Interaction):
             if role:
                 users_with_role = [member.mention for member in role.members if member != interaction.user]
                 users_with_role_str = ' '.join(users_with_role)
-                embed = discord.Embed(title=f"{interaction.user.name} is looking to play {game}",
-                                      description=f"{users_with_role_str}\n\n{interaction.user.mention} is waiting at voice: {voice_channel.mention}")
+                embed = discord.Embed(
+                    title=f"{interaction.user.name} is looking to play {game}",
+                    description=f"{users_with_role_str}\n\n{interaction.user.mention} is waiting at voice: {voice_channel.mention}"
+                )
 
-                # Get the channel using its ID
                 channel = bot.get_channel(1156180116024590396)
-
-                # Acknowledge the interaction
                 await interaction.response.defer()
-
-                # Send the message to the specific channel
-                message = await channel.send(embed=embed)
-
+                await channel.send(embed=embed)
             else:
-                # Get the channel using its ID
-                channel = bot.get_channel(1156180116024590396)
-
-                # Acknowledge the interaction
                 await interaction.response.defer()
-
-                # Send the message to the specific channel
-                await channel.send("You need to be in a voice channel to use this command. " + interaction.user.mention)
-
-                # Auto delete the message after 10 seconds
+                channel = bot.get_channel(1156180116024590396)
+                message = await channel.send("You need to be in a voice channel to use this command. " + interaction.user.mention)
                 await asyncio.sleep(10)
-                async for message in channel.history(limit=1):
-                    await message.delete()
-        else:
-            # Get the channel using its ID
-            channel = bot.get_channel(1155946685676146709)
-
-            # Acknowledge the interaction
-            await interaction.response.defer()
-
-            # Send the message to the specific channel
-            await channel.send("You need to be in a voice channel to use this command. " + interaction.user.mention)
-
-            # Auto delete the message after 10 seconds
-            await asyncio.sleep(10)
-            async for message in channel.history(limit=1):
                 await message.delete()
+        else:
+            await interaction.response.defer()
+            channel = bot.get_channel(1155946685676146709)
+            message = await channel.send("You need to be in a voice channel to use this command. " + interaction.user.mention)
+            await asyncio.sleep(10)
+            await message.delete()
+
+@bot.command()
+async def add(ctx, name: str, role: str, emoji: str):
+    games[name] = {"style": discord.ButtonStyle.blurple, "role": role, "emoji": emoji}
+    await ctx.send(f"Added new game: {name} with role: {role} and emoji: {emoji}")
 
 keep_alive()
-
-# Running the bot
 bot.run(token)
